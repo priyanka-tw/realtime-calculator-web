a2aa4fb35060b0e1aaad098403e6a4c78bd1168d
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-27 22:41:09 +0530

    add global context provider to share username and socket state across the application

---
+++ src/services/global-context-provider/provider.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import Context from "./context";
+
+const Provider = ({children}) => {
+
+    const [username, setUsername] = useState('');
+    const [socket, setSocket] = useState('');
+
+    return <Context.Provider value={{socket, setSocket, username, setUsername}}>{children}</Context.Provider>
+}
+Provider.propTypes = {
+    children: PropTypes.element,
+}
+
+Provider.defaultProps = {
+    children: null,
+}
+export default Provider;
