431fa691fb79022672f91202b5e55fa3de9b99c3
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-28 02:09:04 +0530

    integrate calculator compenent with home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from "./home.styles";
 import Header from "../../components/header/header";
+import Calculator from "../../components/calculator/calculator";
 
 const Home = () => {
 
     return (
         <Container>
             <Header/>
+            <Calculator/>
         </Container>
     );
 };
--- src/pages/webpage/webpage.test.js
+++ src/pages/webpage/webpage.test.js
@@ -3,8 +3,19 @@ import {render} from '@testing-library/react';
 import 'jest-styled-components';
 import Context from "../../services/global-context-provider/context";
 import Webpage from "./webpage";
+import WS from "jest-websocket-mock";
 
 describe('Webpage', () => {
+    let server, client;
+
+    beforeEach(async () => {
+        server = new WS("ws://localhost:8080/ws", {jsonProtocol: true});
+        client = new WebSocket("ws://localhost:8080/ws");
+    });
+
+    afterEach(() => {
+        WS.clean();
+    });
 
     it('should render login page if the user is not logged in', () => {
         const {container} = render(<Context.Provider value={{username: '', setSocket: jest.fn()}}>
@@ -13,9 +24,10 @@ describe('Webpage', () => {
         expect(container).toMatchSnapshot();
     });
 
-    it('should render home page if the user is logged in', () => {
-        const {container} = render(<Context.Provider value={{username: 'test-username'}}>
+    it('should render home page if the user is logged in', async () => {
+        const {container} = render(<Context.Provider value={{username: 'test-username', socket: client}}>
             <Webpage/> </Context.Provider>);
+        await server.connected;
 
         expect(container).toMatchSnapshot();
     });
