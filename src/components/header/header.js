b52bad730a7e0637b85faf4ff68e3886bfcb5c35
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-28 00:54:20 +0530

    integrate additional data component wth header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {H2, HeaderWrapper, Icon} from "./header.style";
 import {LABELS} from '../../constants/constants';
 import img from '../../assets/calculator.svg';
+import Context from "../../services/global-context-provider/context";
+import AdditionalData from "./additional-data/additional-data";
 
 const Header = () => {
+
+    const {username} = useContext(Context);
 
     return (
         <HeaderWrapper>
             <Icon src={img}/>
             <H2>{LABELS.CALCULATOR}</H2>
+            {username && <AdditionalData/>}
         </HeaderWrapper>
     );
 };
--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import 'jest-styled-components';
+import WS from "jest-websocket-mock";
 import Header from "./header";
+import Context from "../../services/global-context-provider/context";
 
 describe('Header', () => {
+    let server, client;
+
+    beforeEach(async () => {
+        server = new WS("ws://localhost:8080/ws", {jsonProtocol: true});
+        client = new WebSocket("ws://localhost:8080/ws");
+    });
+
+    afterEach(() => {
+        WS.clean();
+    });
     it('should render the component', () => {
         const {container} = render(<Header/>);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should render additional data only when user is logged in', async () => {
+        const {container} = render(<Context.Provider value={{username: 'test-username', socket: client}}>
+            <Header/></Context.Provider>);
+        await server.connected;
 
         expect(container).toMatchSnapshot();
     });
