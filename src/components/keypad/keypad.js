57b1f3bf76e2ddeff19ddd1818e73b40c97625cb
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-28 00:16:45 +0530

    add keypad component which is collection of calc-button component

---
+++ src/components/keypad/keypad.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Column, Row} from "./keypad.styles";
+import CalcButton from "../calc-button/calc-button";
+import SignIn from "../sign-in/sign-in";
+
+const Keypad = ({onClick}) => {
+
+    return (
+        <>
+            <Column>
+                <Row>
+                    <CalcButton value="AC" onClick={onClick}/>
+                    <CalcButton value="C" onClick={onClick}/>
+                    <CalcButton value="%" onClick={onClick}/>
+                    <CalcButton value="/" isOperation onClick={onClick}/>
+                </Row>
+                <Row>
+                    <CalcButton value="7" onClick={onClick}/>
+                    <CalcButton value="8" onClick={onClick}/>
+                    <CalcButton value="9" onClick={onClick}/>
+                    <CalcButton value="*" isOperation onClick={onClick}/>
+                </Row>
+                <Row>
+                    <CalcButton value="4" onClick={onClick}/>
+                    <CalcButton value="5" onClick={onClick}/>
+                    <CalcButton value="6" onClick={onClick}/>
+                    <CalcButton value="-" isOperation onClick={onClick}/>
+                </Row>
+                <Row>
+                    <CalcButton value="1" onClick={onClick}/>
+                    <CalcButton value="2" onClick={onClick}/>
+                    <CalcButton value="3" onClick={onClick}/>
+                    <CalcButton value="+" isOperation onClick={onClick}/>
+                </Row>
+                <Row>
+                    <CalcButton value="0" onClick={onClick} isZero/>
+                    <CalcButton value="." onClick={onClick}/>
+                    <CalcButton value="=" isEqualTo onClick={onClick}/>
+                </Row>
+            </Column>
+        </>
+    );
+};
+
+SignIn.props = {
+    onClick: PropTypes.func.isRequired
+};
+
+export default Keypad;
---
+++ src/components/keypad/keypad.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {act, fireEvent, render} from '@testing-library/react';
+import 'jest-styled-components';
+import Keypad from "./keypad";
+
+describe('Keypad', () => {
+    const mockFn = jest.fn();
+
+    it('should render the component with default prop', () => {
+        const {container} = render(<Keypad onClick={mockFn}/>);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should perform the given action on click of a button', async () => {
+        const {getByTestId} = render(<Keypad onClick={mockFn}/>);
+
+        await act(async () => {
+            await fireEvent.click(getByTestId('digit-button-1'));
+        });
+
+        expect(mockFn).toHaveBeenCalledTimes(1);
+        expect(mockFn).toHaveBeenCalledWith("1");
+    });
+
+});
