25c3135cf13d4831d0cb429d0ddf3adf1d13fd56
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-28 02:58:23 +0530

    integrate history panel component with home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Container from "./home.styles";
 import Header from "../../components/header/header";
 import Calculator from "../../components/calculator/calculator";
+import HistoryPanel from "../../components/history-panel/history-panel";
 
 const Home = () => {
 
     return (
         <Container>
             <Header/>
-            <Calculator/>
+            <div style={{display: "flex"}}>
+                <Calculator/>
+                <HistoryPanel/>
+            </div>
         </Container>
     );
 };
